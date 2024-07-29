6a0d4654db68d5b2cd1f6f765cf15890e13afb5b
psychonomadic <dnajauny@gmail.com>
2024-07-29 03:52:45 -0400

    too late 4 this

--- deploy-commands.js
+++ deploy-commands.js
@@ -9,10 +9,10 @@ const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-	// Grab all the command files
+	// Grab all the command files 
 	const commandsPath = path.join(foldersPath, folder);
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	// Grab the SlashCommandBuilder#toJSON() output of each command's data
+	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
@@ -24,21 +24,21 @@ for (const folder of commandFolders) {
 	}
 }
 
+// Construct and prepare an instance of the REST module
 const rest = new REST().setToken(token);
 
+// Deploy commands
 (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-		// The put method is used to fully refresh all commands in the guild with the current set
-		await rest.put(
+		const data = await rest.put(
 			Routes.applicationCommands(clientId),
 			{ body: commands },
 		);
 
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
-		// And of course, make sure you catch and log any errors!
 		console.error(error);
 	}
 })();
